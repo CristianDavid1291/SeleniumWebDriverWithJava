5274eec0292737cebc82080a2beeb90ff4c7899b
Cristian David <cristiandavid1291@gmail.com>
2022-04-15 12:31:08 -0500

    src/AddItemsIntoCart/Base.java: Amazon question, adding items into cart dinamically

--- src/AddItemsIntoCart/Base.java
+++ src/AddItemsIntoCart/Base.java
@@ -1,9 +1,44 @@
 package AddItemsIntoCart;
 
+import java.util.Arrays;
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
 public class Base {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws InterruptedException {
+
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+
+		// expected
+		String[] itemNames = { "Cucumber", "Brocolli" };
+
+		driver.get("https://rahulshettyacademy.com/seleniumPractise/#/");
+		Thread.sleep(3000);
+
+		List<WebElement> products = driver.findElements(By.cssSelector("h4.product-name"));
+		
+		// convert array into arraylist for easier search
+		List listNames = Arrays.asList(itemNames);
+
+		for (int i = 0; i < products.size(); i++) {
+
+			String[] productName = products.get(i).getText().split("-");
+
+			if (listNames.contains(productName[0].trim())) {
+
+				driver.findElements(By.xpath("//div[@class='product-action']/button")).get(i).click();
+				
+			}
+
+		}
 
 	}
 
