fbea5fb7dc495802a0a547867b50995d38c2b0b9
Cristian David <cristiandavid1291@gmail.com>
2022-04-19 09:57:10 -0500

    src/user_interface_components/ActionsDemo.java: Mouse actions code

---
+++ src/user_interface_components/ActionsDemo.java
@@ -0,0 +1,30 @@
+package user_interface_components;
+
+import java.awt.RenderingHints.Key;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class ActionsDemo {
+
+	public static void main(String[] args) {
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://www.amazon.com/");
+		Actions a = new Actions(driver);
+		
+		//Moves to specific element
+		a.moveToElement(driver.findElement(By.id("nav-link-accountList"))).contextClick().build().perform();
+		a.moveToElement(driver.findElement(By.id("twotabsearchtextbox"))).click().keyDown(Keys.SHIFT).sendKeys("hello").doubleClick().build().perform();
+		
+		
+
+	}
+
+}
