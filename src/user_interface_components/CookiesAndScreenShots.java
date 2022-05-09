4121c8d4d410a3a442c09bb7051f5a586ce036f7
Cristian David <cristiandavid1291@gmail.com>
2022-05-09 11:40:34 -0500

    src/user_interface_components/CookiesAndScreenShots.java

---
+++ src/user_interface_components/CookiesAndScreenShots.java
@@ -0,0 +1,27 @@
+package user_interface_components;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class CookiesAndScreenShots {
+
+	public static void main(String[] args) throws IOException {
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().deleteAllCookies();
+		driver.get("https://www.google.com/");
+		File src = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+		FileUtils.copyFile(src, new File("F:\\CRISTIAN\\Eclipse\\Introduction\\screenshot.png"));
+
+	}
+
+}
