d9e081446e64031dc87983adc3ecf44dd1d706dc
Cristian David <cristiandavid1291@gmail.com>
2022-05-10 10:37:15 -0500

    src/TaskAsigments/TaskJavaStream.java

--- src/TaskAsigments/TaskJavaStream.java
+++ src/TaskAsigments/TaskJavaStream.java
@@ -14,17 +14,38 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 public class TaskJavaStream {
 
 	public static void main(String[] args) {
-		
+
 		WebDriverManager.chromedriver().setup();
 		WebDriver driver = new ChromeDriver();
 		driver.get("https://rahulshettyacademy.com/seleniumPractise/#/offers");
 		driver.findElement(By.xpath("//th[@role='columnheader' and contains(@aria-label,'Veg/fruit')]")).click();
 		List<WebElement> tableElements = driver.findElements(By.xpath("//tr/td[1]"));
-		
-		List<String> originalList = tableElements.stream().map(s->s.getText()).collect(Collectors.toList());
+
+		List<String> originalList = tableElements.stream().map(s -> s.getText()).collect(Collectors.toList());
 		List<String> sortedList = originalList.stream().sorted().collect(Collectors.toList());
 		Assert.assertTrue(originalList.equals(sortedList));
 
+		////scan the name of column with getText -> Beans -> print the price
+		////custom function with lambda
+		List<String> price;
+		do {
+			List<WebElement> rows = driver.findElements(By.xpath("//tr/td[1]"));
+			price = rows.stream().filter(s -> s.getText().contains("Rice"))
+					.map(s -> getPriceVeggie(s)).collect(Collectors.toList());
+			price.stream().forEach(s -> System.out.println(s));
+			
+			if(price.size() < 1) {
+				driver.findElement(By.cssSelector("a[aria-label='Next']")).click();
+			}
+			
+		}while (price.size() < 1);
+		
+
+	}
+
+	private static String getPriceVeggie(WebElement s) {
+		String priceValue = s.findElement(By.xpath("following-sibling::td[1]")).getText();
+		return priceValue;
 	}
 
 }
