63bebc936ad43c6b533f53b6875abcd2f4fb1e6a
Cristian David <cristiandavid1291@gmail.com>
2022-05-11 16:59:00 -0500

    src/LambadaExpressions/Suppliers.java suppliers

---
+++ src/LambadaExpressions/Suppliers.java
@@ -0,0 +1,15 @@
+package LambadaExpressions;
+
+import java.util.Date;
+import java.util.function.Supplier;
+
+public class Suppliers {
+
+	public static void main(String[] args) {
+		
+		Supplier<Date> s=()->new Date();
+		System.out.println(s.get());
+
+	}
+
+}
